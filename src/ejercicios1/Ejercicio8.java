0aaaf71ea5a5f12fdfd8bb3f50645d543cc9600d
YerayJ <yerayjimeenez07@gmail.com>
2023-02-23 13:36:30 +0100

    EJERCICIO 8 terminado

--- src/ejercicios1/Ejercicio8.java
+++ src/ejercicios1/Ejercicio8.java
@@ -23,20 +23,14 @@ public class Ejercicio8 {
 
         // Comprueba si el prefijo está en idioma Javalandia
         if (frase.startsWith(PREFIJO)) {
-            // Recorre la tabla empezando por la cantidad de carácteres del sufijo
-            for (int i = PREFIJO.length(); i < frase.length(); i++) {
-                // Le va añadiendo las letras a la fraseTraducida
-                fraseTraducida += frase.charAt(i);
-            }
+            // Coge la subcadena sin el prefijo
+            fraseTraducida = frase.substring(PREFIJO.length());
             // Muestra la frase traducida
             System.out.println("Frase traducida:" + fraseTraducida);
             // Comprueba que la frase termine en el sufijo
         } else if (frase.endsWith(SUFIJO)) {
-            // Recorre la tabla terminando en la posición anterior a donde empieza el sufijo
-            for (int i = 0; i < frase.indexOf(SUFIJO) - 1; i++) {
-                // Comprueba que la frase termine en el sufijo
-                fraseTraducida += frase.charAt(i);
-            }
+            // Coge la subcadena sin el prefijo
+            fraseTraducida = frase.substring(0,frase.length()-SUFIJO.length());
             // Muestra la frase traducida
             System.out.println("Frase traducida: " + fraseTraducida);
             // En caso de que no contenga ni el prefijo ni el sufijo, muestra que no está en ese idioma
